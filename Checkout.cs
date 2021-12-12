58842d5b4bf2ef353a9db21cd71058b65057f85f
Ashraf Patel <4shr4f@live.co.uk>
2021-12-12 14:42:50 +0000

    first commit

---
+++ Checkout.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AseessmentFirstCentral
+{
+    public class Checkout
+    {
+        private decimal total;
+        public decimal Total()
+        {
+            return 0m;
+        }
+        public void Scan(Item item)
+        {
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Activities;
+using System.Activities.Statements;
+using System.Linq;
+
+namespace AseessmentFirstCentral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
