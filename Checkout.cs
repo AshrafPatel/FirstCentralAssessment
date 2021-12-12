59a668bbe3b975898acf904a092f2eb7f59f5c0f
Ashraf Patel <4shr4f@live.co.uk>
2021-12-12 18:33:07 +0000

    done creating scenario in main based on example

--- Checkout.cs
+++ Checkout.cs
@@ -6,61 +6,62 @@ using System.Threading.Tasks;
 
 namespace AseessmentFirstCentral
 {
-    public class Checkout
-    {
-        public List<Item> Items { get; set; }
-        public decimal Price { get; set; }
-        public decimal Total()
-        {
-            foreach (Item item in Items)
-            {
-                Item y;
+	public class Checkout
+	{
+		public List<Item> Items { get; set; }
+		public decimal Price { get; set; }
+		public decimal Total()
+		{
+			foreach (Item item in Items)
+			{
+				foreach (Offer y in Offer.Offers)
+				{
+					if (y.SKU == item.SKU)
+					{
+						if (item.Quantity >= y.Quantity)
+						{
+							int remainder = item.Quantity % y.Quantity;
 
-                //if (Offer.Offers.Any(y =>
-                {
-                    if (item.Quantity >= y.Quantity)
+							if (remainder > 0)
+							{
+								decimal floatNumber = item.Quantity / y.Quantity;
+								int howManyOfferPrice = (int)(floatNumber - Math.Truncate(floatNumber));
+
+								Price += howManyOfferPrice * y.OfferPrice;
+								Price += item.Price * remainder;
+							}
+							else
+							{
+								int howManyOfferPrice = item.Quantity / y.Quantity;
+								Price += howManyOfferPrice * y.OfferPrice;
+							}
+						}
+						else
+						{
+							Price += item.Price * item.Quantity;
+						}
+					}
+					else if (Offer.Offers.FindIndex(i => i.SKU == item.SKU) < 0)
                     {
-                        int remainder = item.Quantity % y.Quantity;
+						Price += item.Price * item.Quantity;
+					}
+				}
+			}
 
-                        if (remainder > 0)
-                        {
-                            decimal floatNumber = item.Quantity / y.Quantity;
-                            int howManyOfferPrice = (int)(floatNumber - Math.Truncate(floatNumber));
+			return Price;
+		}
+		public void Scan(Item item)
+		{
+			foreach (Item i in Items)
+			{
+				if (i.SKU == item.SKU)
+				{
+					i.Quantity += 1;
+					return;
+				}
+			}
 
-                            Price += howManyOfferPrice * y.Price;
-                            Price += item.Price * remainder;
-                        }
-                        else
-                        {
-                            int howManyOfferPrice = item.Quantity / y.Quantity;
-                            Price += howManyOfferPrice * y.Price;
-                        }
-                    }
-                    else
-                    {
-                        Price += item.Price * item.Quantity;
-                    }
-                } 
-                else
-                {
-                    Price += item.Price * item.Quantity;
-                }
-            }
-
-            return Price;
-        }
-        public void Scan(Item item)
-        {
-            foreach (Item i in Items)
-            {
-                if (i.SKU == item.SKU)
-                {
-                    i.Quantity += 1;
-                    return;
-                }
-            }
-
-            Items.Add(item);
-        }
-    }
+			Items.Add(item);
+		}
+	}
 }
